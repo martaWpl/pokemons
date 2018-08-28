4b999dee8ecc5be80caf83dedb9a38b15a148035
wisniem7 <marta_164@vp.pl>
2018-08-28 22:49:22 +0200

    refractoring, code cleaned

--- src/app/components/pokemon-details/pokemon-details.component.ts
+++ src/app/components/pokemon-details/pokemon-details.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {PokemonService} from "../../services/pokemon.service";
 import {ActivatedRoute, Params, Router} from "@angular/router";
 import {IPokemon} from "../../interfaces/pokemon";
@@ -10,42 +10,45 @@ import {IPokemon} from "../../interfaces/pokemon";
 })
 export class PokemonDetailsComponent implements OnInit {
   private pokemonId: Params;
-  pokemon: IPokemon;
-  loading: boolean;
+  private pokemon: IPokemon;
+  private loading: boolean;
   private error: boolean;
 
   constructor(private pokemonService: PokemonService, private route: ActivatedRoute, private router: Router) {
     this.error = false;
+    //reload page after router's parameter change
     this.router.routeReuseStrategy.shouldReuseRoute = () => false;
+
+    //get pokemon Id using url
     this.route.params.subscribe(pokemon => {
-      if(pokemon.id) {
-        this.pokemonId= pokemon.id;
-        console.log(pokemon);
+      if (pokemon.id) {
+        this.pokemonId = pokemon.id;
       } else {
-        console.log('Nie ma takiego pokemona!');}
+        this.error = true;
+        console.warn("Can't route pokemon's id!");
+      }
     });
     this.loading = true;
   }
 
+  /*arrows navigation */
+  changeCard(id: number) {
+    if (id) {
+      this.router.navigate([`/pokemons/${id}`]);
+    } else {
+      console.warn('Error: incorrect id!');
+    }
+  }
+
   ngOnInit() {
+    /*get pokemon's data based on pokemonId*/
     this.pokemonService.getPokemon(this.pokemonId).subscribe(
       (pokemon) => {
-        console.log(pokemon);
         this.pokemon = pokemon;
         this.loading = false;
       }, (error2 => {
-        console.log(error2);
         this.error = true;
+        console.warn(error2);
       }))
-  }
-
-  changeCard(id: number) {
-    console.log(id);
-    if(id) {
-      this.router.navigate([`/pokemons/${id}`]);
-      this.ngOnInit();
-    } else {
-      console.log('Nie działa!');
-    }
   }
 }
--- src/app/components/pokemon-search/pokemon-search.component.ts
+++ src/app/components/pokemon-search/pokemon-search.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {PokemonService} from "../../services/pokemon.service";
 import {Router} from "@angular/router";
 import {IPokemons} from "../../interfaces/pokemons";
@@ -11,55 +11,58 @@ import {Observable, range} from "rxjs/index";
   styleUrls: ['./pokemon-search.component.scss']
 })
 export class PokemonSearchComponent implements OnInit {
-  pokemonId: string;
+  private pokemonId: string;
   private pokemons: Array<IPokemon>;
-  currentPageIndex: number;
+  private currentPageIndex: number;
   private error: boolean;
 
-  constructor(private router: Router, private pokemonService:PokemonService) {
+  constructor(private router: Router, private pokemonService: PokemonService) {
     this.pokemonId = "";
     this.currentPageIndex = 1;
     this.error = false;
   }
 
-  ngOnInit() {
-    this.getPokemons();
-  }
-
-  getPokemonIndex(url) {
-    if(url) {
-      url=url.split('/');
-      return url[url.length-2];
-    }
-  }
-
+  /* get a list of pokemons for current page in pagination*/
   getPokemons() {
-    console.log(this.currentPageIndex);
-    this.pokemonService.getPokemons(this.currentPageIndex).subscribe(value => {
+    this.pokemonService.getPokemons(this.currentPageIndex).subscribe(
+      value => {
         this.pokemons = value.results;
         this.pokemons.map(pokemon => {
-          pokemon.id = this.getPokemonIndex(pokemon.url);
-          pokemon.appUrl= `pokemons/${pokemon.id}`;
+          pokemon.id = PokemonSearchComponent.getPokemonIndex(pokemon.url);
+          pokemon.appUrl = `pokemons/${pokemon.id}`;
         });
       },
       error2 => {
+        console.warn(error2);
         this.error = true;
         this.pokemonId = "";
       })
   }
+
+  /* get an index based on url*/
+  static getPokemonIndex(url) {
+    if (url) {
+      url = url.split('/');
+      return url[url.length - 2];
+    }
+  }
+
+  /*get a pokemon based on id*/
   searchPokemon(pokemonId: string) {
     let id = parseInt(pokemonId);
-    if(id) {
+    if (id) {
       this.router.navigate([`/pokemons/${id}`]);
     } else {
       this.error = true;
     }
   }
-
+  /* pagination */
   changePage(i) {
     this.currentPageIndex = i;
     this.getPokemons();
-    console.log(this.pokemons);
-    console.log('Page, changed!');
+  }
+
+  ngOnInit() {
+    this.getPokemons();
   }
 }
